5c30fbd7731d6c3b08562c5bca2112f7679baa7c
Taha <taha.sifeddin@gmail.com>
2025-08-26 22:48:31 +0200

    asdf

---
+++ app.py
@@ -0,0 +1,14 @@
+import torch
+from TTS.api import TTS
+import gradio as gr
+
+device = "coda" if torch.cuda.is_available() else "cpu"
+
+def generate_audio(text="Das ist ein Text"):
+    tts = TTS(model_name='tts_models/de/thorsten/vits').to(device)
+    tts.tts_to_file(text=text,file_path="outputs/output.wav")
+    return "outputs/output.wav"
+
+demo = gr.Interface(fn=generate_audio,inputs=[gr.Text(label="Text"),],outputs=[gr.Audio(label="Audio")])
+
+demo.launch()
--- main.py
+++
@@ -1,4 +0,0 @@
-from TTS.api import TTS
-tts_manager = TTS().list_models()
-all_models = tts_manager.list_models()
-print(all_models)
